2caded34055abedc25c9273b3df6906740be5ac1
riccardo-ottavi <ottavi.ps@libero.it>
2026-02-03 23:45:26 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+function App() {
+  
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route/>
+      </Routes>
+    </BrowserRouter>
+      <h1>Prova</h1>
+    </>
+  )
+}
+
+export default App
