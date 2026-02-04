4f7d36a30bcc34817cf6371c754b4d96723291da
riccardo-ottavi <ottavi.ps@libero.it>
2026-02-04 17:27:07 +0100

    inizializza ricerca pokemon

--- src/pages/PokeListPage.jsx
+++ src/pages/PokeListPage.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useState, useEffect } from "react"
 import { GlobalContext } from "../contexts/PokeContext"
 import ListCard from "../components/ListCard";
 import { Link } from "react-router-dom";
@@ -6,10 +6,14 @@ import { Link } from "react-router-dom";
 export default function PokeListPage(){
 
     const { pokeList } = useContext(GlobalContext);
+    const [query, setQuery] = useState("")
+
+    const filteredList = pokeList?.filter((p) => p?.title?.toLowerCase().includes(query))
 
     return(
         <div className="list-container">
-            {pokeList.map((poke) => (
+            <input type="text" onChange={(e) => setQuery(e.target.value)}/>
+            {filteredList.map((poke) => (
                 <Link to={`/pokemons/${poke.id}`}  key={poke.id}>
                     <ListCard 
                         poke={poke}
