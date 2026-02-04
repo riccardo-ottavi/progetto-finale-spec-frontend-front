0edb4340b40628ff41c5b827b9050fb521bdee81
riccardo-ottavi <ottavi.ps@libero.it>
2026-02-04 14:12:38 +0100

    inizializza stile della pagina lista

--- src/pages/PokeListPage.jsx
+++ src/pages/PokeListPage.jsx
@@ -8,8 +8,10 @@ export default function PokeListPage(){
     return(
         <div>
             {pokeList.map((poke) => (
-                <div key={poke.id}>
+                <div className="small-card" key={poke.id}>
+                    <img src={`/images/${poke.title.toLowerCase()}.png`} alt={poke.title} />
                     <p>{poke.title}</p>
+                    <p>{poke.category}</p>
                 </div>
             ))}
         </div>
