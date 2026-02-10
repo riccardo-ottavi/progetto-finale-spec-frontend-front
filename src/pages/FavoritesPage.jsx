06d3596556f4b61399dbe329b6f992cfacedc065
riccardo-ottavi <ottavi.ps@libero.it>
2026-02-10 10:06:09 +0100

    refactoring pagina referiti

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -6,9 +6,12 @@ export default function FavoritesPage(){
 
     const {favorites, pokeList} = useContext(GlobalContext)
 
+    const getPokemonById = (id) => pokeList.find(p => p.id === id);
+
     //recupera i dati (no dettaglio) a partire dai singoli id
     const favPokeList = favorites.map((favId) => (
-        pokeList.find(p => p.id === favId)
+        //pokeList.find(p => p.id === favId)
+        getPokemonById(id)
     ))
 
     return(
